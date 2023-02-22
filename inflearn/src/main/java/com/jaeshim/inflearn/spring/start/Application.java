62d3eeded939f172b35f869773d058bcb506a60f
jaeshim <jaeshim@gmarket.com>
2023-02-22 23:44:00 +0900

    initial commit

---
+++ inflearn/src/main/java/com/jaeshim/inflearn/spring/start/Application.java
@@ -0,0 +1,13 @@
+package com.jaeshim.inflearn.spring.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
