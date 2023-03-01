7353bae50cf5ea35940a8b5d55570db0bddb9d9d
jaeshim <jaeshim@gmarket.com>
2023-03-01 21:25:54 +0900

    20230301

--- inflearn/src/main/java/com/jaeshim/inflearn/spring/start/SpringConfig.java
+++ inflearn/src/main/java/com/jaeshim/inflearn/spring/start/SpringConfig.java
@@ -1,21 +1,34 @@
 package com.jaeshim.inflearn.spring.start;
 
+import com.jaeshim.inflearn.spring.start.repository.JdbcMemberRepository;
+import com.jaeshim.inflearn.spring.start.repository.JdbcTemplateMemberRepository;
 import com.jaeshim.inflearn.spring.start.repository.MemberRepository;
 import com.jaeshim.inflearn.spring.start.repository.MemoryMemberRepository;
 import com.jaeshim.inflearn.spring.start.service.MemberService;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
+  private DataSource dataSource;
+
+  @Autowired
+  public SpringConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
   @Bean
-  public MemberService memberService(){
+  public MemberService memberService() {
     return new MemberService(memberRepository());
   }
 
   @Bean
-  public MemberRepository memberRepository(){
-    return new MemoryMemberRepository();
+  public MemberRepository memberRepository() {
+//    return new MemoryMemberRepository();
+//    return new JdbcMemberRepository(dataSource);
+    return new JdbcTemplateMemberRepository(dataSource);
   }
 }
